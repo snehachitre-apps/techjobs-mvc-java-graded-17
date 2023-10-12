132725e55aa189e299d0a7ec33ca623027505a7b
snehachitre-apps <sneha.chitre.usa@gmail.com>
2023-10-12 01:31:56 -0500

    workimg as expected but all tests failing

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +28,27 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+
+    //getting radio button name in request param parwanthesis gets the correct column name
+    @PostMapping(value="results")
+    public String displaySearchResults(Model model, @RequestParam("searchType") String column, @RequestParam(required = false) String searchTerm)
+    {
+        ArrayList<Job> jobs;
+        if (column.equals("all")){
+            jobs = JobData.findAll();
+            System.out.println("searching all" + jobs);
+
+
+        } else {
+            jobs = JobData.findByColumnAndValue(column, searchTerm);
+
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+
+    }
 
 }
 
